488773b86200b8d493e20ee2a78c1a33c30a79f0
Sheldelraze <minh891996@gmail.com>
2017-03-26 23:50:49 +0700

    Add files via upload
    + Add file class diagram
    * Fix more database

--- Database.sql
+++ Database.sql
@@ -10,8 +10,8 @@ create table if not exists project_management.faculty (
 	#id(10) = toi da 10 cs
 	faculty_id int (10) not null auto_increment,
     faculty_enable bool not null,
-    faculty_dean nvarchar(30) not null,
-    faculty_vice_dean nvarchar(30) not null,
+    faculty_dean_name nvarchar(30) not null,
+    faculty_vice_dean_name nvarchar(30) not null,
     faculty_major nvarchar(30) not null,
     faculty_lecturer_count int(10) not null,
     faculty_student_count int(10) not null,
@@ -28,9 +28,9 @@ create table if not exists project_management.classes (
     classes_enable bool not null,
     classes_name nvarchar(10) not null,
 	classes_student_count int(10) not null,
-    classes_representative nvarchar(30) not null,
-    classes_vice_representative nvarchar(30) not null,
-    classes_head_teacher nvarchar(30) not null,
+    classes_representative_name nvarchar(30) not null,
+    classes_vice_representative_name nvarchar(30) not null,
+    classes_head_teacher_name nvarchar(30) not null,
     classes_academic_year int(2) not null,
     classes_faculty_id int(10) not null,
     foreign key(classes_faculty_id) references project_management.faculty(faculty_id) on delete cascade on update cascade,
@@ -46,6 +46,7 @@ create table if not exists project_management.user (
     user_gender nvarchar(10) not null,
     user_identification_number varchar(12) not null,
     user_language nvarchar(15) not null,
+    user_nationality nvarchar(15),
     user_salary int (10),
     user_address nvarchar(100),
     user_hometown nvarchar(20),
@@ -101,23 +102,23 @@ create table if not exists project_management.project (
     project_name nvarchar(30) not null,
     project_author_id int(10) not null,
     project_instructor_id int(10) not null,
+    project_content_link varchar (100) not null,
+    project_progress text(1000) not null,
     #diem danh gia
     project_valuation_point float(5,2),
     #danh gia = chu
     project_appraisal text(1000),
     project_enable bool not null,
     project_start_date datetime,
-    project_post_date datetime,
     #text: toi da 65536 ki tu
     project_brief text(2000),
     project_view_count int(3),
-    project_download_count int(3),
+	project_page_count int(10),
+    project_chapter_list nvarchar(500),
     project_font varchar(20),
     project_language nvarchar(20),
     #project_size: mb
     project_size float(10,2),
-    project_page_count int(10),
-    project_chapter_count int(5),
     project_last_modified datetime,
     #hoi dong cham diem
     project_valuation_council text(1000),
@@ -145,12 +146,57 @@ create table if not exists project_management.comment (
     comment_post_date datetime,
     comment_enable boolean not null,
     comment_last_modifed datetime,
-    comment_last_editor nvarchar(30),
+    comment_last_editor_id int(10),
     comment_is_read boolean,
     comment_font varchar(30),
     comment_language nvarchar(30),
     comment_project_contain_id int(10),
+    comment_parent_id int(10),
+    foreign key (comment_last_editor_id) references project_management.user(user_id) on delete cascade on update cascade,
     foreign key (comment_project_contain_id) references project_management.project(project_id) on delete cascade on update cascade,
     foreign key (comment_poster_id) references project_management.user(user_id) on delete cascade on update cascade,
     primary key(comment_id)
 ); 
+
+DROP TABLE IF EXISTS project_management.form ;
+
+create table if not exists project_management.form (
+	form_id int(10) not null auto_increment,
+    form_title nvarchar (100) not null,
+    form_creator_id int (10) not null,
+    form_receiver_id int (10) not null,
+	form_created_date datetime,
+    form_status nvarchar(20) not null,
+	foreign key (form_receiver_id) references project_management.user(user_id) on delete cascade on update cascade,
+    foreign key (form_creator_id) references project_management.user(user_id) on delete cascade on update cascade,
+    primary key (form_id)
+);
+DROP TABLE IF EXISTS project_management.project_create_form ;
+
+create table if not exists project_management.project_create_form (
+	project_create_form_id int(10) not null,
+    project_create_form_project_name nvarchar(50) not null,
+    project_create_form_project_brief text(1000) not null, 
+    foreign key(project_create_form_id) references project_management.form(form_id) on delete cascade on update cascade,
+    primary key(project_create_form_id)
+) ;
+
+DROP TABLE IF EXISTS project_management.progress_report_form ;
+
+create table if not exists project_management.progress_report_form (
+	progress_report_form_id int(10) not null,
+    progress_report_form_content text(1000) not null,
+    progress_report_form_project_id int(10) not null,
+    foreign key (progress_report_form_project_id) references project_management.project(project_id) on delete cascade on update cascade,
+    foreign key (progress_report_form_id) references project_management.form(form_id) on delete cascade on update cascade,
+    primary key(progress_report_form_id)
+);
+
+DROP TABLE IF EXISTS project_management.delete_project_form;
+
+create table if not exists project_management.delete_project_form (
+	delete_project_form_id int(10) not null,
+	delete_project_form_reason text(1000) not null,
+    foreign key (delete_project_form_id) references project_management.form(form_id) on delete cascade on update cascade,
+    primary key(delete_project_form_id)
+)
